56ea294caad9e2638dd1fee03d74b9a3bd72670b
sreejanagireddy <sreejanagireddy589@gmail.com>
2017-03-25 02:54:30 +0530

    Create pwd.py

---
+++ assignments/shell/cmd_pkg/pwd.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import shutil
+"""
+COMMAND NAME    :    pwd(present working directory)
+DESCRIPTION     :    It is used to display the path of current working directory
+"""
+def pwd():
+	print(os.getcwd())
