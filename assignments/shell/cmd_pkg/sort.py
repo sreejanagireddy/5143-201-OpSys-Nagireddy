79f7f35d094ccb1e0fb64c3ca3a6852ddf160ee9
sreejanagireddy <sreejanagireddy589@gmail.com>
2017-03-25 03:33:14 +0530

    Update sort.py

--- assignments/shell/cmd_pkg/sort.py
+++ assignments/shell/cmd_pkg/sort.py
@@ -9,23 +9,23 @@ def sort(file,flag):
         with open(file,'r') as r:
 		      list=[]
 		      if(flag==1):
-            for line in sorted(r):
-              list.append(line)
+            	      	for line in sorted(r):
+              			list.append(line)
 			
-			    fl=open("default.txt",'w')
-			    for lines in list:
+			 	fl=open("default.txt",'w')
+			 for lines in list:
 				      list.append(lines)
 		      else:	
 			      for line in sorted(r):
-                        	print line
+                              		print line
 def sortredirect(file1,file2):
          with open(file1,'r') as r:
 		            f2=open(file2,'w')
 
-                for line in sorted(r):
-                       f2.write(line)
+                            for line in sorted(r):
+                           	 f2.write(line)
 def sortapp(file1,file2):
          with open(file1,'r') as r:
 		            f2=open(file2,'a')
-                for line in sorted(r):
-                       f2.write(line)
+                            for line in sorted(r):
+                           	 f2.write(line)
