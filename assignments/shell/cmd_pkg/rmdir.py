99c85083a037d97653890739ab2df543767b2cf8
sreejanagireddy <sreejanagireddy589@gmail.com>
2017-03-25 02:57:13 +0530

    Create rmdir.py

---
+++ assignments/shell/cmd_pkg/rmdir.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import shutil
+"""
+COMMAND NAME      :  rmdir
+DESCRIPTION       :  To remove the directory. 
+PARAMETERS        :   Directory
+"""
+def rmdir(cmd):
+        os.rmdir(cmd)
