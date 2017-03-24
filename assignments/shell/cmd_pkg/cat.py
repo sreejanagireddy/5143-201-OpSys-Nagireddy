288cea661ed41bf88347049845e47ff10b1246e7
sreejanagireddy <sreejanagireddy589@gmail.com>
2017-03-25 02:10:49 +0530

    Update cat.py

--- assignments/shell/cmd_pkg/cat.py
+++ assignments/shell/cmd_pkg/cat.py
@@ -11,17 +11,16 @@ PARAMETERS        :  file
 
 def cat(file,flag):
 		
-      if(flag==1):
-			  f = open(file,'r')
-			  f1=open("default.txt",'w')
+      	if(flag==1):
+      		f = open(file,'r')
+		f1=open("default.txt",'w')
+                for line in f:
+			f1.write(line)
             		
-			   for line in f:
-				    f1.write(line)
-            		
-		  else:
-		 	  if(os.path.isfile(file)):
-				    f = open(file,'r')
-            print(f.read())	
+        else:
+		 if(os.path.isfile(file)):
+		 	f = open(file,'r')
+                        print(f.read())	
 def catredirect(file1,file2):
         f=open(file1,'r')
         f1=open(file2,'w')
@@ -31,4 +30,4 @@ def catapp(file1,file2):
         f1=open(file2,'a')
         #shutil.copy(file1,file2)
 	      for lines in f:
-	            f1.write(lines)
+	      	f1.write(lines)
