3d1a4471c52146d687c6d6164a926c17bb37a30e
sreejanagireddy <sreejanagireddy589@gmail.com>
2017-03-25 02:56:11 +0530

    Create rm.py

---
+++ assignments/shell/cmd_pkg/rm.py
@@ -0,0 +1,10 @@
+import sys
+import os
+"""
+COMMAND NAME      :  rm  (remove)
+DESCRIPTION       :  To remove the file from directory.
+PARAMETERS        :  file
+"""
+
+def remove(file):
+        os.remove(file)
